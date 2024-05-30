2cf1c33523dc14d3e09f857619eda68b00294947
cgm <501704164@qq.com>
2024-05-30 17:30:55 +0800

    update accumulate and warmup

--- ops/augmentations/geometric/functional.py
+++ ops/augmentations/geometric/functional.py
@@ -1,8 +1,6 @@
 import cv2
 import numpy as np
-from typing import Sequence, Tuple, List
-from albumentations.core.bbox_utils import denormalize_bbox, normalize_bbox
-import random
+from typing import Sequence, Tuple
 
 
 def shift_scale_rotate_matrix(
@@ -75,67 +73,3 @@ def resize_boxes(boxes, original_size, new_size):
     ratio_height = new_size[0] / original_size[0]
     ratio_width = new_size[1] / original_size[1]
     return resize_boxes_ratio(boxes, ratio_height, ratio_width)
-
-
-def mosaic4(image_batch: List[np.ndarray], height: int, width: int, fill_value: int = 0):
-    """Arrange the images in a 2x2 grid. Images can have different shape.
-    This implementation is based on YOLOv5 with some modification:
-    https://github.com/ultralytics/yolov5/blob/932dc78496ca532a41780335468589ad7f0147f7/utils/datasets.py#L648
-
-    Args:
-        image_batch (List[np.ndarray]): image list. The length should be 4.
-        height (int): Height of output mosaic image
-        width (int): Width of output mosaic image
-        fill_value (int): padding value
-
-    """
-    if len(image_batch) != 4:
-        raise ValueError(f"Length of image_batch should be 4. Got {len(image_batch)}")
-
-    mosaic_border = [-height // 4, -width // 4]
-    yc, xc = (int(random.uniform(-x, y + x)) for x, y in zip(mosaic_border, [height, width]))
-    img4 = np.full((height, width, 3), fill_value, dtype=np.uint8)  # base image with 4 tiles
-    padw_cache = []
-    padh_cache = []
-    for i, img in enumerate(image_batch):
-        (h, w) = img.shape[:2]
-
-        if i == 0:  # top left
-            x1a, y1a, x2a, y2a = max(xc - w, 0), max(yc - h, 0), xc, yc  # xmin, ymin, xmax, ymax (large image)
-            x1b, y1b, x2b, y2b = w - (x2a - x1a), h - (y2a - y1a), w, h  # xmin, ymin, xmax, ymax (small image)
-        elif i == 1:  # top right
-            x1a, y1a, x2a, y2a = xc, max(yc - h, 0), min(xc + w, width), yc
-            x1b, y1b, x2b, y2b = 0, h - (y2a - y1a), min(w, x2a - x1a), h
-        elif i == 2:  # bottom left
-            x1a, y1a, x2a, y2a = max(xc - w, 0), yc, xc, min(height, yc + h)
-            x1b, y1b, x2b, y2b = w - (x2a - x1a), 0, w, min(y2a - y1a, h)
-        elif i == 3:  # bottom right
-            x1a, y1a, x2a, y2a = xc, yc, min(xc + w, width), min(height, yc + h)
-            x1b, y1b, x2b, y2b = 0, 0, min(w, x2a - x1a), min(y2a - y1a, h)
-
-        img4[y1a:y2a, x1a:x2a] = img[y1b:y2b, x1b:x2b]  # img4[ymin:ymax, xmin:xmax]
-        padw_cache.append(x1a - x1b)
-        padh_cache.append(y1a - y1b)
-
-    return img4, padh_cache, padw_cache
-
-
-def bbox_mosaic4(bbox: Tuple, padh: int, padw: int, height: int, width: int):
-    """Put the given bbox in one of the cells of the 2x2 grid.
-    Args:
-        bbox (tuple): A bounding box `(x_min, y_min, x_max, y_max)` or `(x_min, y_min, x_max, y_max, label, ...)`.
-        rows (int): Height of input image that corresponds to one of the mosaic cells
-        cols (int): Width of input image that corresponds to one of the mosaic cells
-        position_index (int): Index of the mosaic cell. 0: top left, 1: top right, 2: bottom left, 3: bottom right
-        height (int): Height of output mosaic image
-        width (int): Width of output mosaic image
-    """
-    # bbox = denormalize_bbox(bbox, height, width)
-    bbox, tail = bbox[:4], tuple(bbox[4:])
-    bbox = (
-        max(bbox[0] + padw, 0),
-        max(bbox[1] + padh, 0),
-        min(bbox[2] + padw, width),
-        min(bbox[3] + padh, height),
-    )
-    return tuple(bbox + tail)
--- ops/augmentations/transforms.py
+++ ops/augmentations/transforms.py
@@ -1,8 +1,11 @@
-from typing import Dict, Any
+from typing import Dict, Any, Tuple
 
 import cv2
 import numpy as np
-from albumentations.core.transforms_interface import ImageOnlyTransform
+
+from albumentations import ImageOnlyTransform, DualTransform
+from albumentations.core.bbox_utils import denormalize_bbox, normalize_bbox
+
 import ops.augmentations.functional as F
 import ops.cv.io as io
 
@@ -71,17 +74,154 @@ class SaltPepperNoise(ImageOnlyTransform):
         return "color", "n", "noise_scale", "border_scale", "saline"
 
 
-if __name__ == '__main__':
-    image = io.imread(r"D:\cgm\dataset\VOC2007\JPEGImages\000005.jpg")
-    print(image.shape)
-    for _ in range(5):
-        x0, y0, x1, y1 = 25, 12, 430, 310
+class Mosaic(DualTransform):
+    """Mosaic augmentation arranges randomly selected four images into single one like the 2x2 grid layout.
 
-        var = SaltPepperNoise(p=1, salience=True, border_scale=0)(image=image,
-                                                                  bboxes=np.array([[x0, y0, x1, y1]], dtype=float))
-        var1 = cv2.rectangle(var['image'].copy(),
-                             var['bboxes'][0, [0, 1]].astype(int),
-                             var['bboxes'][0, [2, 3]].astype(int),
-                             (255, 255, 0), 1)
-        print(var1.shape)
-        io.show('ad', var1)
+    Note:
+        This augmentation requires additional helper targets as sources of additional
+        image and bboxes.
+        The targets are:
+        - `image_cache`: list of images or 4 dimensional np.nadarray whose first dimension is batch size.
+        - `bboxes_cache`: list of bounding boxes. The bounding box format is specified in `bboxes_format`.
+        You should make sure that the bounding boxes of i-th image (image_cache[i]) are given by bboxes_cache[i].
+
+        Here is a typical usage:
+        ```
+        data = transform(image=image, image_cache=image_cache)
+        # or
+        data = transform(image=image, image_cache=image_cache, bboxes=bboxes, bboxes_cache=bboxes_cache)
+        ```
+
+        You can set `image_cache` whose length is less than 3. In such a case, the same image will be selected
+        multiple times.
+        Note that the image specified by `image` argument is always included.
+
+    Args:
+        height (int)): height of the mosaiced image.
+        width (int): width of the mosaiced image.
+        fill_value (int): padding value.
+        replace (bool): whether to allow replacement in sampling or not. When the value is `True`, the same image
+            can be selected multiple times. When False, the length of `image_cache` (and `bboxes_cache`) should
+            be at least 3.
+            This replacement rule is applied only to `image_cache`. So, if the `image_cache` contains the same image as
+            the one specified in `image` argument, it can make image that includes duplication for the `image` even if
+            `replace=False` is set.
+        bboxes_forma (str)t: format of bounding box. Should be on of "pascal_voc", "coco", "yolo".
+
+    Targets:
+        image, mask, bboxes, image_cache, mask_cache, bboxes_cache
+
+    Image types:
+        uint8, float32
+
+    Reference:
+    [Bochkovskiy] Bochkovskiy A, Wang CY, Liao HYM. （2020） "YOLOv 4 : Optimal speed and accuracy of object detection.",
+    https://arxiv.org/pdf/2004.10934.pdf
+
+    """
+
+    def __init__(
+            self,
+            height,
+            width,
+            fill_value=0,
+            always_apply=False,
+            p=0.5,
+    ):
+        super().__init__(always_apply=always_apply, p=p)
+        self.height = height
+        self.width = width
+        self.fill_value = fill_value
+        self.__target_dependence = {}
+
+    def __call__(self, *args, force_apply: bool = False, **kwargs) -> Dict[str, Any]:
+        if args:
+            raise KeyError("You have to pass data to augmentations as named arguments, for example: aug(image=image)")
+        self.update_target_dependence(**kwargs)
+        return super().__call__(force_apply=force_apply, **kwargs)
+
+    @property
+    def target_dependence(self) -> Dict:
+        return self.__target_dependence
+
+    @target_dependence.setter
+    def target_dependence(self, value):
+        self.__target_dependence = value
+
+    def update_target_dependence(self, **kwargs):
+        """Update target dependence dynamically."""
+        self.target_dependence = {}
+        if "image" in kwargs:
+            self.target_dependence["image"] = {"image_cache": kwargs["image_cache"]}
+        if "mask" in kwargs:
+            self.target_dependence["mask"] = {"mask_cache": kwargs["mask_cache"]}
+        if "bboxes" in kwargs:
+            self.target_dependence["bboxes"] = {"bboxes_cache": kwargs["bboxes_cache"],
+                                                "classes_cache": kwargs['classes_cache']}
+
+    def apply(self, image, image_cache=None, height=0, width=0, fill_value=114, **params):
+        image_cache.append(image)
+        image, self.padh_cache, self.padw_cache = F.mosaic4(image_cache, height, width, fill_value)
+        return image
+
+    def apply_to_mask(self, mask, mask_cache=None, height=0, width=0, fill_value=114, **params):
+        mask_cache.append(mask)
+        mask, *_ = F.mosaic4(mask_cache, height, width, fill_value)
+        return mask
+
+    def apply_to_bbox(self, bbox, padh=0, padw=0, height=0, width=0, **params):
+        return F.bbox_mosaic4(bbox, padh, padw, height, width)
+
+    def apply_to_bboxes(
+            self,
+            bboxes,
+            bboxes_cache=None,
+            classes_cache=None,
+            height=0,
+            width=0,
+            **params
+    ):
+        new_bboxes = []
+        padh1, padw1 = self.padh_cache.pop(-1), self.padw_cache.pop(-1)
+
+        for bbox, classes, padh, padw in zip(bboxes_cache, classes_cache, self.padh_cache, self.padw_cache):
+            for box, cls in zip(bbox, classes):
+                new_bbox = self.apply_to_bbox(tuple(tuple(box) + tuple([cls])), padh, padw, height, width)
+                new_bbox = normalize_bbox(new_bbox, height, width)
+                new_bboxes.append(new_bbox)
+
+        for box in bboxes:
+            box = denormalize_bbox(box, params['rows'], params['cols'])
+            new_bbox = self.apply_to_bbox(box, padh1, padw1, height, width)
+            new_bbox = normalize_bbox(new_bbox, height, width)
+            new_bboxes.append(new_bbox)
+
+        return new_bboxes
+
+    def apply_to_keypoint(self, **params):
+        pass  # TODO
+
+    def get_params(self) -> Dict[str, Any]:
+        return {
+            "height": self.height,
+            "width": self.width,
+            "fill_value": self.fill_value,
+        }
+
+    def get_transform_init_args_names(self) -> Tuple[str, ...]:
+        return "height", "width", "fill_value", "bboxes_cache_format"
+
+# if __name__ == '__main__':
+#     image = io.imread(r"D:\cgm\dataset\VOC2007\JPEGImages\000005.jpg")
+#     print(image.shape)
+#     for _ in range(5):
+#         x0, y0, x1, y1 = 25, 12, 430, 310
+#
+#         var = SaltPepperNoise(p=1, salience=True, border_scale=0)(image=image,
+#                                                                   bboxes=np.array([[x0, y0, x1, y1]], dtype=float))
+#         var1 = cv2.rectangle(var['image'].copy(),
+#                              var['bboxes'][0, [0, 1]].astype(int),
+#                              var['bboxes'][0, [2, 3]].astype(int),
+#                              (255, 255, 0), 1)
+#         print(var1.shape)
+#         io.show('ad', var1)
