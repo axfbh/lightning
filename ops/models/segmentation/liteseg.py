63c6a0e9d02c66b4a5f4f0a7b4ebcce1d0be7ec0
cgm <501704164@qq.com>
2024-06-07 17:22:20 +0800

    update accumulate and warmup

--- ops/models/segmentation/liteseg.py
+++ ops/models/segmentation/liteseg.py
@@ -2,6 +2,7 @@ import torch.nn as nn
 from torchvision.models.mobilenetv3 import mobilenet_v3_large
 from ops.models.backbone.utils import _mobilenet_extractor
 from ops.models.misc.atrous import ASPP
+from torchvision.ops.misc import Conv2dNormActivation
 
 
 class LiteSeg(nn.Module):
@@ -10,6 +11,8 @@ class LiteSeg(nn.Module):
         self.backbone = _mobilenet_extractor(mobilenet_v3_large(pretrained=True), 1)
 
         self.aspp = ASPP(1280, 96, [3, 6, 9])
+
+        self.conv = Conv2dNormActivation(96 * 5 + 1280, 96, 1)
 
         self.head = nn.Conv2d(96, num_classes, kernel_size=1)
 
