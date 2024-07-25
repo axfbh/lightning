4f2127485a4e34f69c17de29655bc97c55c9ddbf
cgm <501704164@qq.com>
2024-07-25 11:25:40 +0800

    update yolo tal

--- ops/dataset/utils.py
+++ ops/dataset/utils.py
@@ -61,8 +61,8 @@ class DataCache:
     def _set_cache(self, anno_paths, read_fn, *args):
         self.annotations = []
 
-        for anno in anno_paths:
-            self.annotations.append(read_fn(anno, *args))
+        for i, anno in enumerate(anno_paths):
+            self.annotations.append(read_fn(anno, i, *args))
 
     def __getitem__(self, item):
         anno = self.annotations[item]
