5a681d5fc1fe0244340b5322908c8668acc791d7
cgm <501704164@qq.com>
2024-07-25 16:10:18 +0800

    update yolo tal

--- ops/metric/ClassificationMetric.py
+++ ops/metric/ClassificationMetric.py
@@ -8,46 +8,48 @@ from math import isnan
 class ClassificationMetric:
     def __init__(self, num_classes):
         self.num_classes = num_classes
+        self.accuracy = 0
+        self.info_matrix = {'precision': 0,
+                            'recall': 0,
+                            'f1-score': 0}
+        self.num = 0
 
-    def fit(self, input: torch.Tensor, target: torch.Tensor):
-        self.input = torch.softmax(input, 1).argmax(1).cpu().numpy()
-        self.target = target.numpy()
-        if self.num_classes > 2:
-            report = metrics.classification_report(self.target,
-                                                   self.input,
-                                                   output_dict=True,
-                                                   zero_division=np.nan)
-            self.accuracy = report['accuracy']
-            self.weighted_avg = report['weighted avg']
+    def add_batch(self, input, target):
+        report = metrics.classification_report(target,
+                                               input,
+                                               output_dict=True,
+                                               zero_division=np.nan)
+        self.accuracy += report['accuracy']
+        self.num += 1
 
-    def accuracy_score(self):
-        if self.num_classes > 2:
-            return self.accuracy
-        return metrics.accuracy_score(self.target, self.input)
+        weighted_avg = report['weighted avg']
+        for key in self.info_matrix.keys():
+            self.info_matrix[key] += weighted_avg[key]
+
+    def reset(self):
+        self.info_matrix = np.zeros(3)
+        self.num = 0
 
     def precision_score(self):
         """
         提升精确率是为了不错报,精确率越高越好
         :return:
         """
-        if self.num_classes > 2:
-            return self.weighted_avg['precision']
-        return metrics.precision_score(self.target, self.input)
+        return self.info_matrix['precision'] / self.num
 
     def recall_score(self):
         """
         提升召回率是为了不漏报, 召回率越高，代表实际坏用户被预测出来的概率越高
         :return:
         """
-        if self.num_classes > 2:
-            return self.weighted_avg['recall']
-        return metrics.recall_score(self.target, self.input)
+        return self.info_matrix['recall'] / self.num
+
+    def accuracy_score(self):
+        return self.accuracy / self.num
 
     def f1_score(self):
         """
         精确率和召回率的调和平均值, F1 score越高，说明模型越稳健
         :return:
         """
-        if self.num_classes > 2:
-            return self.weighted_avg['f1-score']
-        return metrics.f1_score(self.target, self.input)
+        return self.info_matrix['f1-score'] / self.num
