54f53694cd74eded63f371340e9e3866200d7083
cgm <501704164@qq.com>
2024-10-17 11:07:23 +0800

    update yolo loss

--- train.py
+++ train.py
@@ -1,7 +1,6 @@
 import argparse
-from ops.utils.logging import print_args
 from ops.models.detection.yolo.trainer import Yolo
 
 if __name__ == '__main__':
     model = Yolo("./cfg/models/yolo/v5/yolov5.yaml")
-    model.train(data="./cfg/datasets/voc.yaml", imgsz=[640, 640], batch=5, box=0.05)
+    model.train(data="./cfg/datasets/voc.yaml", imgsz=[640, 640], batch=8, box=0.05)
