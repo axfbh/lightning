92e3e72d011e1af7a804ca226ad073ea981ea362
cgm <501704164@qq.com>
2024-10-25 16:53:08 +0800

    update yolo head

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 from ops.models.detection.yolo.trainer import Yolo
 
 if __name__ == '__main__':
-    model = Yolo("yolov4s.yaml")
-    model.train(data="./cfg/datasets/voc.yaml", imgsz=[640, 640], epochs=220, batch=8, box=0.05)
-
+    model = Yolo("yolov4n.yaml")
+    # model.train(data="./cfg/datasets/voc.yaml", imgsz=[640, 640], epochs=220, batch=8, box=0.05)
+    # yolov4
+    model.train(data="./cfg/datasets/voc.yaml", imgsz=[640, 640], epochs=220, batch=8, box=0.15, cls=1.5, obj=3)
